d373a7d5b94f3089fe553447051bbacec8d99db5
Máximo Tovar <maximo.tovar@laika.pet>
2024-09-02 02:29:42 -0400

    cambiando el orden de los high order components

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,11 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "../context/UserProvider";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <BrowserRouter>
     <UserProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </UserProvider>
-  </StrictMode>
+  </BrowserRouter>
 );
